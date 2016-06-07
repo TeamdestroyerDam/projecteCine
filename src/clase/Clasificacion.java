91d51fdcdf10d373c13aadddaa738a50296f820d
DAM <DAM@usuario-HP>
2016-06-07 09:03:47 +0200

    02/06/2016 10:17

---
+++ src/clase/Clasificacion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase;
+
+/**
+ *
+ * @author DAM
+ */
+public enum Clasificacion {
+    Todos_los_publicos,Mayores_de_7,Mayores_de_12,Mayores_de_16,Mayores_de_18;
+}
---
+++ src/clase/Genero.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase;
+
+/**
+ *
+ * @author DAM
+ */
+public enum Genero {
+    Comedia,Terror,Drama,Accion,Aventura,CienciaFiccion,Romantico,Musical,Melodrama,Suspense,Fantasia;
+}
